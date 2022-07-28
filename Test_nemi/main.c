6a9b9098d1c9ac2efdee8674ac0dec4969c5ced9
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-28 19:03:06 +0000

    My working directory space

---
+++ Test_nemi/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+int main(int argc, char **argv)
+{
+	// Load config files, if any.
+
+	// Run command loop.
+	void shl_loop();
+	// Perform any shutdown/cleanup.
+	return (EXIT_SUCCESS);
+}
---
+++ Test_nemi/shl_read_line.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+char *shl_read_line()
+{
+	size_t position = 0;
+	size_t buffSize = 32;
+	int characters = '\0';
+	char *buffer = malloc(sizeof(char) * buffSize);
+
+	if (!buffer)
+	{
+		perror("shl: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while (1)
+	{
+		/* read a character */
+		characters = getline(&buffer, &buffSize, stdin);
+		if (getline(&buffer, &buffSize, stdin) == -1)
+			{
+				if (feof(stdin))
+					exit(EXIT_SUCCESS);
+				else
+				{
+					perror("readline");
+					exit(EXIT_FAILURE);
+				}
+			}
+		/* If we hit EOF, replace it with a null charatcer and return */
+		if (characters == EOF || characters == '\n')
+		{
+			buffer[position] = '\0';
+			return (buffer);
+		}
+		else
+			buffer[position] = characters;
+	}
+	return (buffer);
+}
