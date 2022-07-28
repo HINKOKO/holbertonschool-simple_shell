5cb311751e076606ce581bd6e730a286655823ab
root <piotr62@live.fr>
2022-07-28 02:15:39 -0700

    Little modif

--- main.h
+++ main.h
@@ -1,6 +1,9 @@
 #ifndef __SIMPLE_SHELL__
 #define __SIMPLE_SHELL__
 
+#define IN 1
+#define OUT 0
+
 char **get_args(char *buff);
 size_t wordcount(char *s);
 int main(int ac, char **av, char **env);
