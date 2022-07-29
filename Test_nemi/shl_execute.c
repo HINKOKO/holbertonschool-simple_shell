924ee03d258e88062752ce5c08798b6e062e7d49
root <piotr62@live.fr>
2022-07-29 02:50:53 -0700

    sendind exec draft to nehemie

--- Test_nemi/shl_execute.c
+++ Test_nemi/shl_execute.c
@@ -1,7 +1,25 @@
 #include "main.h"
 
-int shl_execute(char **args __attribute__((unused)))
+int shl_execute(char **args)
 {
-	return(0);
-}
+	pid_t child;
 
+	child = fork();
+	if (child == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			perror(args[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (child > 0)
+	{
+		wait(NULL);
+	}
+	else
+	{
+		return (1);
+	}
+	return (0);
+}
